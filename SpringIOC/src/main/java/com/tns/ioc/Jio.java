3a11ee0eff80f3895e1e2b6d9a2cfa7e31d3a624
amey <amey@LAPTOP-GHF5MGC8>
2022-06-12 20:09:12 +0530

    First commit

---
+++ SpringIOC/src/main/java/com/tns/ioc/Jio.java
@@ -0,0 +1,18 @@
+package com.tns.ioc;
+
+public class Jio implements Sim {
+public Jio() {
+		
+		System.out.println("Jio Class Constructor");
+	}
+	public void calling() {
+		System.out.println("Jio calling");
+		
+	}
+
+	public void data() {
+		System.out.println("Jio Data-pack");
+		
+	}
+
+}
